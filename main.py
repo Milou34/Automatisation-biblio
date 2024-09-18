022e7689db5be8c034c734afa69f4a33be7a8cb8
Milou34 <ma-r-ylou@hotmail.fr>
2024-09-18 22:18:28 +0200

    espe D

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import xml.etree.ElementTree as ET
 from openpyxl import Workbook
 import fnmatch
 import os
-from src.infosGenerales import extract_general_info, create_table
+from src.especesDeterminantes import process_esp_D
+from src.infosGenerales import adjust_columns, extract_general_info, create_table
 from src.habitats import process_habitats
 
 def main(folder_source):
@@ -17,13 +18,18 @@ def main(folder_source):
 
     # Créer un nouveau fichier Excel
     wb = Workbook()
-    ws = wb.active
-    
-    # Liste des chemins de balises pour extraire les informations générales
-    tag_paths = ['NM_SFFZN', 'LB_ZN', 'TY_ZONE', 'SU_ZN']
 
-    # Déterminer la ligne actuelle pour créer le tableau
-    current_row = 1
+    # Créer des feuilles distinctes pour ZNIEFF 1 et ZNIEFF 2
+    ws_znieff1 = wb.active
+    ws_znieff1.title = "ZNIEFF 1"
+    ws_znieff2 = wb.create_sheet(title="ZNIEFF 2")
+
+    # Liste des chemins de balises pour extraire les informations générales
+    tag_paths = ['NM_SFFZN', 'LB_ZN', 'SU_ZN']
+
+    # Initialiser la ligne actuelle pour chaque type de ZNIEFF
+    current_row_znieff1 = 1
+    current_row_znieff2 = 1
 
     # Parcourir les fichiers XML trouvés
     for fichier in fichiers_xml:
@@ -31,24 +37,37 @@ def main(folder_source):
         tree = ET.parse(fichier)
         root = tree.getroot()
 
-        # Ajouter le premier tableau
+        # Déterminer le type de ZNIEFF à partir de la balise TY_ZONE
+        type_znieff = int(root.find('TY_ZONE').text)  # On suppose que TY_ZONE existe et est valide
+
+        # Sélectionner la feuille et la ligne en fonction du type de ZNIEFF
+        if type_znieff == 1:
+            ws = ws_znieff1
+            current_row = current_row_znieff1
+        elif type_znieff == 2:
+            ws = ws_znieff2
+            current_row = current_row_znieff2
+        else:
+            # Si TY_ZONE n'est pas 1 ou 2, on passe à l'itération suivante
+            continue
+
+        # Ajouter le premier tableau pour les informations générales
         current_row = create_table(ws, "Informations générales", 
-                     ['ID national', 'Nom ZNIEFF', 'Type ZNIEFF', 'Surface totale ZNIEFF'],
+                     ['ID national', 'Nom ZNIEFF', 'Surface totale ZNIEFF (Ha)'],
                      current_row)
-        
+
         # Extraction des données depuis le fichier XML
         infos_value = extract_general_info(root, tag_paths)
         # Convertir les valeurs numériques pour l'affichage correct
-        infos_value[3] = float(infos_value[3].replace(',', '.'))  # Conversion de 'Surface totale ZNIEFF'
-        infos_value[2] = int(infos_value[2])  # Conversion de 'Type ZNIEFF'
+        infos_value[2] = float(infos_value[2].replace(',', '.'))  # Conversion de 'Surface totale ZNIEFF'
         infos_value[0] = int(infos_value[0])  # Conversion de 'ID national'
         ws.append(infos_value)
         ws.append([])
         current_row += 2  # Mettre à jour la ligne après avoir ajouté le premier tableau
 
-        # Ajouter le deuxième tableau
+        # Ajouter le deuxième tableau pour les habitats
         current_row = create_table(ws, "Habitats déterminants",
-                     ['EUNIS', 'CORINE biotopes', 'Habitats d’intérêt communautaire', 'Source', 'Observation'],
+                     ['EUNIS', 'CORINE biotopes', 'Habitats d’intérêt communautaire', 'Source', 'Surface en Ha', 'Observation'],
                      current_row)
 
         # Parcourir les balises TYPO_INFO_ROW pour récupérer les habitats
@@ -58,27 +77,35 @@ def main(folder_source):
             # On ne garde que les habitats avec FG_TYPO = "D"
             if fg_typo == "D":
                 current_row = process_habitats(typo_info_row, ws, current_row)
+                
+        current_row = create_table(ws, "Espèces déterminantes",
+                     ["Groupe", "Code espèce", "Nom scientifique", "Nom vernaculaire", "Statut(s) biologique(s)", "Sources", "Degré d'abondance", "Effectif inférieur estimé", "Effectif supérieur estimé", "Année d'observation"],
+                     current_row)
+        
+        for espece_row in root.findall('.//ESPECE_ROW'):
+            fg_esp = espece_row.find('FG_ESP').text
+
+            # On ne garde que les habitats avec FG_TYPO = "D"
+            if fg_esp == "D":
+                current_row = process_esp_D(espece_row, ws, current_row)
 
         # Ajoute une ligne vide entre chaque fichier XML
         ws.append([])
         current_row += 1  # Mettre à jour la ligne après avoir ajouté une ligne vide
 
-        # Modifier le titre de la feuille en fonction du type ZNIEFF
-        type_znieff = int(root.find('TY_ZONE').text)  # Assurez-vous que type_znieff est défini ici
+        # Mettre à jour la ligne courante pour le type de ZNIEFF
         if type_znieff == 1:
-            ws.title = "ZNIEFF 1"
+            current_row_znieff1 = current_row
         elif type_znieff == 2:
-            ws.title = "ZNIEFF 2"
+            current_row_znieff2 = current_row
 
     # Sauvegarder le fichier Excel
+    adjust_columns(wb)
     wb.save(os.path.join(folder_source, 'Récap.xlsx'))
     print(f"Fichier Excel '{folder_source}/Récap.xlsx' généré avec succès !")
 
 # Demander les chemins des fichiers à l'utilisateur
 # folder_source = input("Entrez le chemin du dossier des XML sources, le Excel sera créé dans ce dossier : ")
 
-
 # Lancer la conversion XML vers Excel
-main("C:\\Users\\MarylouBERTIN\\OneDrive - Grive Environnement\\Bureau\\Test 2")
-
-# xml_to_excel(folder_source)
+main("C:\\Users\\MarylouBERTIN\\OneDrive - Grive Environnement\\Bureau\\Test")
